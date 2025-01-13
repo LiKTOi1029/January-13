261cf05a5096fe4211fe7fec44f7a83c268715fc
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-13 23:53:30 +0330

    added a bit more functionality to ArrayRotator.lua:table.randomizer(tab)

--- ArrayRotator.lua
+++ ArrayRotator.lua
@@ -1,10 +1,15 @@
+math.randomseed(os.time())
+
 tLines = {}
-function table.randomizer(table)
-	local randomInt = math.random(#tLines)
-	return randomInt
+function table.randomizer(tab)
+	local tNewLines = {}
+	local tKeys = {}
+	for num1, num2 in pairs(tab) do
+		table.insert(tKeys, num1)
+	end
+	return tNewLines
 end
 
 for num0 in io.lines("output.txt") do
 	table.insert(tLines, num0)
-	print(table.randomizer(tLines) .. " | " .. num0)
 end
